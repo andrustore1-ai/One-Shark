2a9339d43407267c687f618560666a55c07979dd
andrustore1-ai <andrustore1@gmail.com>
2026-05-15 00:20:33 +0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,30 @@
+// firebase-config.js
+
+const firebaseConfig1885 = {
+  apiKey: "AIzaSyCHg8UZV5iJh3O9fog4iSkrsQi4STugcrs",
+  authDomain: "kaood-6f44d.firebaseapp.com",
+  databaseURL: "https://kaood-6f44d-default-rtdb.firebaseio.com",
+  projectId: "kaood-6f44d",
+  storageBucket: "kaood-6f44d.firebasestorage.app",
+  messagingSenderId: "374769332160",
+  appId: "1:374769332160:web:914adfe7c9daff7518a225",
+  measurementId: "G-4KBHRH8L57"
+};
+
+const FIREBASE_PATHS_1885 = {
+  settingsStore: "settings1885/store1885",
+  settingsBanners: "settings1885/banners1885",
+  settingsPaymentMethods: "settings1885/paymentMethods1885",
+  categories: "categories1885",
+  products: "products1885",
+  users: "users1885",
+  orders: "orders1885"
+};
+
+const STORAGE_KEYS_1885 = {
+  cart: "tofan_cart_1885",
+  wishlist: "tofan_wishlist_1885",
+  checkoutSelection: "tofan_checkout_selection_1885",
+  selectedCategory: "tofan_selected_category_1885",
+  selectedHomeSection: "tofan_selected_home_section_1885"
+};
